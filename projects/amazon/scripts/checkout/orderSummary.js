c467dd58822ce281759d5f21d4ce871821794fbb
breznaM <breznamatej22@gmail.com>
2026-07-25 08:40:30 +0200

    Minor chanages

--- projects/amazon/scripts/checkout/orderSummary.js
+++ projects/amazon/scripts/checkout/orderSummary.js
@@ -76,11 +76,7 @@ export function renderOrderSummary() {
         const productId = link.dataset.productId;
         removeFromCart(productId);
 
-        const container = document.querySelector(`.js-cart-item-container-${productId}`)
-        container.remove();
-
-        document.querySelector('.js-cart-quantity').innerHTML = `${calculateCartQuantity()} items`
-
+        renderOrderSummary();
         renderPaymentSummary();
       })
     });
