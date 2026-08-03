34689a9c8c7419d9088bb2e8c9ebd9e0bfbc5aa4
breznaM <breznamatej22@gmail.com>
2026-08-03 16:33:30 +0200

    Finish checkout page

--- projects/amazon/data/delivery-options.js
+++ projects/amazon/data/delivery-options.js
@@ -1,3 +1,5 @@
+import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
+
 export const deliveryOptions = [{
   id: '1', 
   deliveryDays: 7,
@@ -22,4 +24,23 @@ export function getDeliveryOption(deliveryOptionId) {
   });
 
   return deliveryOption || deliveryOptions[0];
+}
+
+export function calculateDeliveryDate(deliveryOption) {
+  let deliveryDate = dayjs();
+  let i = 1;
+
+  while (i <= deliveryOption.deliveryDays) {
+    deliveryDate = deliveryDate.add(1, 'days');
+
+    if (isWeekend(deliveryDate)) continue;
+    i++;
+  }
+  
+  return deliveryDate.format('dddd, MMMM D');
+}
+
+function isWeekend(deliveryDate) {
+  if (deliveryDate.day() === 0 || deliveryDate.day() === 6) return true;
+  else return false;
 }
--- projects/amazon/scripts/checkout/orderSummary.js
+++ projects/amazon/scripts/checkout/orderSummary.js
@@ -1,9 +1,9 @@
 import { cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeliveryOption } from '../../data/cart.js';
 import { products, getProduct } from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
-import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
-import { deliveryOptions, getDeliveryOption } from '../../data/delivery-options.js';
+import { deliveryOptions, getDeliveryOption, calculateDeliveryDate } from '../../data/delivery-options.js';
 import { renderPaymentSummary } from './paymentSummary.js';
+import { renderCheckoutHeader } from './checkoutHeader.js';
 
 export function renderOrderSummary() {
   let cartSummaryHTML = '';
@@ -17,14 +17,10 @@ export function renderOrderSummary() {
 
     const deliveryOption = getDeliveryOption(deliveryOptionId);
 
-    const today = dayjs();
-    const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
-    const dateString = deliveryDate.format('dddd, MMMM D');
-
     cartSummaryHTML += `
       <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
-          Delivery date: ${dateString}
+          Delivery date: ${calculateDeliveryDate(deliveryOption)}
         </div>
 
         <div class="cart-item-details-grid">
@@ -68,7 +64,6 @@ export function renderOrderSummary() {
   });
 
   document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
-  document.querySelector('.js-cart-quantity').innerHTML = `${calculateCartQuantity()} items`
 
   document.querySelectorAll('.js-delete-link')
     .forEach((link) => {
@@ -77,6 +72,7 @@ export function renderOrderSummary() {
         removeFromCart(productId);
 
         renderOrderSummary();
+        renderCheckoutHeader();
         renderPaymentSummary();
       })
     });
@@ -139,10 +135,6 @@ export function renderOrderSummary() {
     let html = '';
 
     deliveryOptions.forEach((deliveryOption) => {
-      const today = dayjs();
-      const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
-      const dateString = deliveryDate.format('dddd, MMMM D');
-
       const priceString = deliveryOption.priceCents === 0 ? 'FREE' : `$${formatCurrency(deliveryOption.priceCents)} -`
 
       const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
@@ -154,7 +146,7 @@ export function renderOrderSummary() {
           name="delivery-option-${matchingProduct.id}">
         <div>
           <div class="delivery-option-date">
-            ${dateString}
+            ${calculateDeliveryDate(deliveryOption)}
           </div>
           <div class="delivery-option-price">
             ${priceString} Shipping
